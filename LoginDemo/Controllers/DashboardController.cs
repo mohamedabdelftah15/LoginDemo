2a96369baf150516c7e220d686ba8c6be47142d0
mohamedabdelftah15 <51211389+mohamedabdelftah15@users.noreply.github.com>
2021-02-25 15:35:37 +0200

    Add project files.

---
+++ LoginDemo/Controllers/DashboardController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoginDemo.CustomAttributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginDemo.Controllers
+{
+    [UnAuthorized]
+    public class DashboardController : Controller
+    {
+        [Authorize(Roles.DIRECTOR)]
+        public IActionResult DirectorPage()
+        {
+            ViewBag.UserRole = GetRole();
+            ViewBag.Message = "Permission controlled through Authorize Attribute";
+            return View("DirectorPage");
+        }
+
+        [Authorize(Roles.SUPERVISOR)]
+        public IActionResult SupervisorPage()
+        {
+            ViewBag.UserRole = GetRole();
+            ViewBag.Message = "Permission controlled through Authorize Attribute";
+            return View("SupervisorPage");
+        }
+
+        [Authorize(Roles.ANALYST)]
+        public IActionResult AnalystPage()
+        {
+            ViewBag.UserRole = GetRole();
+            ViewBag.Message = "Permission controlled through Authorize Attribute";
+            return View("AnalystPage");
+        }
+
+        public IActionResult SupervisorAnalystPage()
+        {
+            ViewBag.UserRole = GetRole();
+            ViewBag.Message = "Permission controlled inside action method in-line code";
+            if (this.HavePermission(Roles.SUPERVISOR))
+                return View("SupervisorPage");
+            
+            if (this.HavePermission(Roles.ANALYST))
+                return View("AnalystPage");
+            
+            return new RedirectResult("~/Dashboard/NoPermission");
+        }
+
+        [Authorize(Roles.DIRECTOR, Roles.SUPERVISOR, Roles.ANALYST )]
+        public IActionResult PageLevelPermission()
+        {
+            ViewBag.UserRole = GetRole();
+            return View("PageLevelPermission");
+        }
+
+        public IActionResult CheckAjaxCalls()
+        {
+            ViewBag.UserRole = GetRole();
+            return View("CheckAjaxCalls");
+        }
+
+        public JsonResult AuthenticateAjaxCalls()
+        {
+            return Json(new {result = "success" });
+        }
+
+        [Authorize(Roles.DIRECTOR, Roles.SUPERVISOR)]
+        public JsonResult AuthorizeAjaxCalls()
+        {
+            return Json(new { result = "success" });
+        }
+
+        public IActionResult NoPermission()
+        {
+            ViewBag.UserRole = GetRole();
+            return View("NoPermission");
+        }
+
+        private string GetRole()
+        {
+            if (this.HavePermission(Roles.DIRECTOR))
+                return " - DIRECTOR";
+            if (this.HavePermission(Roles.SUPERVISOR))
+                return " - SUPERVISOR";
+            if (this.HavePermission(Roles.ANALYST))
+                return " - ANALYST";
+            return null;
+        }
+    }
+}
---
+++ LoginDemo/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LoginDemo.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using LoginDemo.CustomAttributes;
+
+namespace LoginDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            User objLoggedInUser = new User();
+            if (User.Identity.IsAuthenticated)
+            {
+                var claimsIndentity = HttpContext.User.Identity as ClaimsIdentity;
+                var userClaims = claimsIndentity.Claims;
+
+                if (HttpContext.User.Identity.IsAuthenticated)
+                {
+                    foreach (var claim in userClaims)
+                    {
+                        var cType = claim.Type;
+                        var cValue = claim.Value;
+                        switch (cType)
+                        {
+                            case "USERID":
+                                objLoggedInUser.USERID = cValue;
+                                break;
+                            case "EMAILID":
+                                objLoggedInUser.EMAILID = cValue;
+                                break;
+                            case "PHONE":
+                                objLoggedInUser.PHONE = cValue;
+                                break;
+                            case "DIRECTOR":
+                                objLoggedInUser.ACCESS_LEVEL = cValue;
+                                break;
+                            case "SUPERVISOR":
+                                objLoggedInUser.ACCESS_LEVEL = cValue;
+                                break;
+                            case "ANALYST":
+                                objLoggedInUser.ACCESS_LEVEL = cValue;
+                                break;
+                        }
+                    }
+                    ViewBag.UserRole = GetRole();
+                }
+            }
+            return View("Index", objLoggedInUser);
+        }
+
+        public IActionResult LoginUser(User user)
+        {
+            TokenProvider _tokenProvider = new TokenProvider();
+            var userToken = _tokenProvider.LoginUser(user.USERID.Trim(), user.PASSWORD.Trim());
+            if (userToken != null)
+            {
+                //Save token in session object
+                HttpContext.Session.SetString("JWToken", userToken);
+            }
+            return Redirect("~/Home/Index");
+        }
+
+        public IActionResult Logoff()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("~/Home/Index");
+        }
+
+        public JsonResult EndSession()
+        {
+            HttpContext.Session.Clear();
+            return Json(new {result = "success"});
+        }
+        private string GetRole()
+        {
+            if (this.HavePermission(Roles.DIRECTOR))
+                return " - DIRECTOR";
+            if (this.HavePermission(Roles.SUPERVISOR))
+                return " - SUPERVISOR";
+            if (this.HavePermission(Roles.ANALYST))
+                return " - ANALYST";
+            return null;
+        }
+    }
+}
